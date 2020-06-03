77af6ee06f43d15a12297f270198419f07eb4b75
Kterlecki <terlecki1995@gmail.com>
2020-06-03 20:33:42 +0000

    added logout function and logout message confirming logout successfull

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth, messages
 def index(request):
     """ Return the index.html file """
     return render(request, 'index.html')
+
+def logout(request):
+    """ log the user out """
+    auth.logout(request)
+    messages.success(request, 'Logout Successfull')
+    return redirect(reverse('index'))
