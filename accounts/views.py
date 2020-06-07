a9a3e3dd08ba514891302bd938fb72ea7b817193
Kterlecki <terlecki1995@gmail.com>
2020-06-07 13:23:45 +0000

    added basic login page with button

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
+from accounts.forms import UserLoginForm
+
 def index(request):
     """ Return the index.html file """
     return render(request, 'index.html')
@@ -9,3 +11,8 @@ def logout(request):
     auth.logout(request)
     messages.success(request, 'Logout Successfull')
     return redirect(reverse('index'))
+
+def login(request):
+    """return a login page"""
+    login_form = UserLoginForm()
+    return render(request, 'login.html', {'login_form': login_form})
