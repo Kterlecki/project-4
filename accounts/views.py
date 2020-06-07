f67c6f26ca910d7d2f9c5f4e80defee7e1888c99
Kterlecki <terlecki1995@gmail.com>
2020-06-07 18:28:12 +0000

    added message when login/logout sucesfull

--- accounts/views.py
+++ accounts/views.py
@@ -14,5 +14,19 @@ def logout(request):
 
 def login(request):
     """return a login page"""
-    login_form = UserLoginForm()
+    if request.method == 'POST':
+        login_form= UserLoginForm(request.POST)
+
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'],
+                                    password=request.POST['password'])
+           
+
+            if user:
+                auth.login(user=user    , request=request)
+                messages.success(request, 'Log in succesfull')
+            else:
+                login_form.add_error(None, 'Your username/password is incorrect')
+    else:
+        login_form = UserLoginForm()    
     return render(request, 'login.html', {'login_form': login_form})
