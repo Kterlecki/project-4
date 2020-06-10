4a384b4128dd2851458756161c875d83d98494c3
Kterlecki <terlecki1995@gmail.com>
2020-06-10 09:04:34 +0000

    added if statement to redirect user to home page when logged in

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 from accounts.forms import UserLoginForm
 
 def index(request):
     """ Return the index.html file """
     return render(request, 'index.html')
 
+@login_required
 def logout(request):
     """ log the user out """
     auth.logout(request)
@@ -14,6 +16,8 @@ def logout(request):
 
 def login(request):
     """return a login page"""
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
     if request.method == 'POST':
         login_form= UserLoginForm(request.POST)
 
@@ -25,6 +29,7 @@ def login(request):
             if user:
                 auth.login(user=user    , request=request)
                 messages.success(request, 'Log in succesfull')
+                return redirect(reverse('index'))
             else:
                 login_form.add_error(None, 'Your username/password is incorrect')
     else:
